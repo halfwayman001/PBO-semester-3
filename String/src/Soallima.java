a5b0a23d5ed93301f366803628e49f10bb72eeab
lubban <79937781+halfwayman001@users.noreply.github.com>
2021-09-24 08:47:52 +0700

    Add files via upload

---
+++ String/src/Soallima.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Soallima {
+
+    public static void main(String[] args) {
+
+        String A,B;
+        Scanner sc=new Scanner(System.in);
+        A=sc.next();
+        B=sc.next();
+
+        System.out.println(A.length()+B.length());
+        if (A.compareTo(B) > 0){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        System.out.printf(A.substring(0,1).toUpperCase()+A.substring(1, A.length()));
+        System.out.println(" " + B.substring(0,1).toUpperCase()+B.substring(1, B.length()));
+
+
+
+    }
+
+
+}
